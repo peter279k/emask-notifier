632c15183cc4ec3f779f06cd8d1292a22c2eac2c
Peter <peter279k@gmail.com>
2021-01-10 02:28:02 +0800

    Let composer.phar to be ignored and supervisorctl

--- notifier_php_builder.sh
+++ notifier_php_builder.sh
@@ -24,10 +24,10 @@ ${sudo_prefix}touch ${supervisor_path}
 
 echo '[program:notifier-php]' | ${sudo_prefix}tee ${supervisor_path}
 echo "command=/bin/bash -c \"cd $PWD && /usr/bin/php7.4 notifier.php\"" | ${sudo_prefix}tee -a ${supervisor_path}
-echo 'autostart=true' | ${sudo_prefix}tee -a ${supervisor_path}
-echo 'autorestart=true' | ${sudo_prefix}tee -a ${supervisor_path}
+echo 'autostart=false' | ${sudo_prefix}tee -a ${supervisor_path}
+echo 'autorestart=false' | ${sudo_prefix}tee -a ${supervisor_path}
+echo 'startretries=0' | ${sudo_prefix}tee -a ${supervisor_path}
 echo "user=$USER" | ${sudo_prefix}tee -a ${supervisor_path}
-echo "events=TICK_60" | ${sudo_prefix}tee -a ${supervisor_path}
 echo 'redirect_stderr=true' | ${sudo_prefix}tee -a ${supervisor_path}
 echo "stdout_logfile=$PWD/notifier-php.log" | ${sudo_prefix}tee -a ${supervisor_path}
 
@@ -38,5 +38,19 @@ if [[ $? != 0 ]]; then
     echo -e "${red_color}The supervisor service is not found. Do you install it?${rest_color}"
     exit 1;
 fi;
+
+echo "${yellow_color}Setting up the Cronjob...${rest_color}"
+
+which crontab
+
+if [[ $? != 0 ]]; then
+    echo -e "${red_color}The crontab is not found. Do you install it?${rest_color}"
+    exit 1;
+fi;
+
+cronjob_path="/etc/cron.d/notifier-php"
+echo 'SHELL=/bin/bash' | ${sudo_prefix}tee ${cronjob_path}
+echo 'PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin' | ${sudo_prefix}tee -a ${cronjob_path}
+echo '* * * * * root supervisorctl start notifier-php' | ${sudo_prefix}tee -a ${cronjob_path}
 
 echo -e "${green_color}Building PHP Notifier has been done!${rest_color}"
